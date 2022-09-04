56ead9e23b00cecb8d5014fca85afda2a33b2637
dlrudf77 <dlrudf77@gmail.com>
2022-09-04 20:44:48 +0900

    ngrox모듈 추가, 엔터 전송, 등등 유튜브 강의에 있는 모든 기능 구현

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /// 서버
 
+var moment = require('moment'); // moment.js 사용하기
+
 const express=require("express")
 const http = require("http")
 const app=express()
@@ -17,7 +19,13 @@ const PORT = process.env.PORT || 5000
 io.on("connection",(socket)=>{
     ///console.log('연결이 이루어 졌습니다')
     socket.on("chatting",(data)=>{
-        io.emit("chatting",data)
+        const{name, msg}=data //deconstructure
+
+        io.emit("chatting",{ //,data 였는데 쪼개서 object로 보내기
+            name:name, //그냥 name, 이렇게 해도됨
+            msg:msg,
+            time:moment().format("hh:mm")
+        })
     })
 })
  
--- src/js/chat.js
+++ src/js/chat.js
@@ -5,19 +5,54 @@ const nickname=document.querySelector("#nickname")
 const chatlist=document.querySelector(".chatting-list")
 const chatinput=document.querySelector(".chatting-input")
 const sendbutton=document.querySelector(".send-button")
+const displaycontainer=document.querySelector(".display-container")
 
-sendbutton.addEventListener("click", ()=>{
-    const param={
+function send() {
+     // 대화명(nickname), 채팅내용(chatting input) 데이터 객체로 저장한다음에
+     const param={
         name:nickname.value,
-        msg:chatinput.value
+        msg:chatinput.value,
     }
+    //서버로 데이터 발사
     socket.emit("chatting", param)
+}
+
+// 전송버튼 클릭했을 때
+sendbutton.addEventListener("click", send) //여기선 send()라고하면 전송버튼이 먹통됨
+
+function enterkey() {
+	if (window.event.keyCode == 13) {
+        send() //()안붙였더니 엔터쳐도 안되더라.. 근데 전송누르고 엔터누르니 됨
+    }
+}
+
+// 서버로부터 data를 받았을때
+socket.on("chatting", (data)=>{
+    const{name,msg,time}=data
+    const item=new LiModel(name, msg, time);
+    item.makeLi()
+    displaycontainer.scrollTo(0,displaycontainer.scrollHeight) //chatting-list로 하니 안됨 ul태그라 그런듯
 })
 
-socket.on("chatting", (data)=>{
-    const li=document.createElement("li");
-    li.innerText=`${data.name}님이 - ${data.msg}`;
-    chatlist.appendChild(li)
-})
+function LiModel(name, msg, time){
+    this.name=name;
+    this.msg=msg;
+    this.time=time;
+
+    this.makeLi=()=>{
+        const li=document.createElement("li" );
+        li.classList.add(nickname.value===this.name ? "sent" : "received")
+        // this.name->받은 이름, nickname.value->지금 내 대화명에 써있는 이름
+
+        const dom= `            <span class="profile">
+                                    <span class="user">${this.name}</span>
+                                    <img class='image' src="http://placeimg.com/50/50/any" alt="any">
+                                </span> 
+                                <span class="message">${this.msg}</span>
+                                <span class="time">${this.time}</span>`
+        li.innerHTML=dom
+        chatlist.appendChild(li)
+    }
+}
 
 ///console.log(socket)
